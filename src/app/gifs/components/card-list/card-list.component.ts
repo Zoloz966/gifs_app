7fd767f3c8ba41089aec3fc6c8191c3f7c1e80c6
Zolouz966 <zrodrigo96@gmail.com>
2023-08-31 11:51:48 -0400

    Añadimos el card component

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -10,7 +10,6 @@ export class CardListComponent {
   @Input()
   public gifs: Gif[] = [];
 
-
 }
 
 
--- src/app/gifs/pages/home/home-page.component.ts
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -4,16 +4,14 @@ import { Gif } from '../../interfaces/gifs.interface';
 
 @Component({
   selector: 'gifs-home-page',
-  templateUrl: './home-page.component.html'
+  templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
 
-  constructor(
-    private gifsService: GifsService,
-  ){}
+  constructor( private gifsService: GifsService ) {}
 
   get gifs(): Gif[] {
-    return this.gifsService.gifsList
+    return this.gifsService.gifsList;
   }
 
 }
