e4c0e89020a0bd97ef3800113ab994d03ab51658
Zolouz966 <zrodrigo96@gmail.com>
2023-08-30 21:58:31 -0400

    Terminamos la página

---
+++ src/app/gifs/components/search-gifs/search-box.component.ts
@@ -0,0 +1,39 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar:</h5>
+    <input
+      #txtTagInput
+      type="text"
+      class="form-control"
+      placeholder="Buscar gifs..."
+      (keyup.enter)="searchTag()"
+    />
+  `,
+})
+export class SearchBoxComponent {
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor(
+    private gifsService: GifsService
+  ) {}
+
+  // searchTag(newTag: string) {
+  //   console.log(newTag);
+  // }
+
+  searchTag(){
+    const newTag = this.tagInput.nativeElement.value;
+
+    this.gifsService.searchTag(newTag)
+
+    this.tagInput.nativeElement.value = ""
+
+  }
+
+}
